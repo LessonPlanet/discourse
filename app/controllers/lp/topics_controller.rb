0c35349a0627e20f11346ec88428821a33683dd8
Jason Rust <jason@lessonplanet.com>
2014-07-30 17:28:45 -0700

    [#72194046] Article Subheaders Should Show on Articles

--- app/controllers/lp/topics_controller.rb
+++ app/controllers/lp/topics_controller.rb
@@ -31,4 +31,30 @@ class Lp::TopicsController < TopicsController
 
     topic.nil? || topic.errors.present? ? render_json_error(topic) : render_serialized(topic, BasicTopicSerializer)
   end
+
+  def update
+    topic_params = {
+        title:          params[:topic_title],
+        skip_callbacks: true
+    }
+
+    topic = Topic.where(id: params[:id]).first
+
+    Topic.transaction do
+      topic.update_attributes topic_params
+      if params[:category].present?
+        category = Category.where(id: params[:category].to_i).first
+        topic.changed_to_category(category) if (category && category != topic.category)
+      end
+    end if topic
+
+    # update post body
+    if topic && params[:topic_body].present?
+      post     = topic.posts.where(user_id: topic.user.id).order(:sort_order).first
+      post.raw = params[:topic_body]
+      post.save
+    end
+
+    topic.nil? || topic.errors.present? ? render_json_error(topic) : render_serialized(topic, BasicTopicSerializer)
+  end
 end
