3ae4544cd3c0f85897c896be081a5f12c380ae37
Jason Rust <jason@lessonplanet.com>
2014-11-21 09:22:15 -0800

    Removing duplicate code

--- app/controllers/lp/topics_controller.rb
+++ app/controllers/lp/topics_controller.rb
@@ -15,26 +15,6 @@ class Lp::TopicsController < TopicsController
   def update
     topic_params = {
         title:          params[:topic_title],
-        sub_header:     params[:topic_sub_header],
-        skip_callbacks: true
-    }
-
-    topic = Topic.where(id: params[:id]).first
-
-    Topic.transaction do
-      topic.update_attributes topic_params
-      if params[:category].present?
-        category = Category.where(id: params[:category].to_i).first
-        topic.changed_to_category(category) if (category && category != topic.category)
-      end
-    end if topic
-
-    topic.nil? || topic.errors.present? ? render_json_error(topic) : render_serialized(topic, BasicTopicSerializer)
-  end
-
-  def update
-    topic_params = {
-        title:          params[:topic_title],
         skip_callbacks: true
     }
 
